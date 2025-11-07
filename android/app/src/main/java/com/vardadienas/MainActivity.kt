d7c180cc21d212351cfb5821eb8e6c707c895a52
Niall Barber <niall.barber@olioex.com>
2025-11-07 19:47:56 +0000

    patch

--- android/app/src/main/java/com/vardadienas/MainActivity.kt
+++ android/app/src/main/java/com/vardadienas/MainActivity.kt
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.zoontek.rnbootsplash.RNBootSplash
 
 import android.os.Bundle;
 
@@ -26,8 +27,4 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(null)
-  }
 }
